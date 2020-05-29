47c05e8d499a72d932aca734b2b4a72de6cd21f6
Ragnabase <Ragnabase@DESKTOP-THQ3I85>
2020-05-29 23:38:32 +0700

    upload Project

---
+++ src/e31192259_queue/E31192259_Queue.java
@@ -0,0 +1,66 @@
+package e31192259_queue;
+import java.util.Scanner;
+public class E31192259_Queue{
+    int id;
+    String nama,perlu;
+    E31192259_Queue next;
+    static Scanner in=new Scanner(System.in);
+    static Scanner str=new Scanner(System.in);
+    //Membuat deklarasi program
+    public void input(){
+        System.out.print("Masukkan id        : ");
+        id=in.nextInt();
+        System.out.print("Masukkan nama      : ");
+        nama=str.nextLine();
+        System.out.print("Masukkan keperluan : ");
+        perlu=str.nextLine();
+        next=null;
+    }
+    //untuk menginput dalam program
+    public void read(){
+        System.out.println("|| "+id+" \t|| "+nama+" \t|| "+perlu+" \t||");
+    }
+    //untuk menampilkan hasil inputan
+    public static void main(String[] args){
+        int menu=0;
+        linked que=new linked();
+        while(menu!=4){
+            System.out.print("1.Enqueue\n2.Dequeue\n3.View\n4.Exit\n : ");
+            menu=in.nextInt();
+            if(menu==1)que.enque();
+            else if(menu==2)que.deque();
+            else if(menu==3)que.view();
+            else if(menu==4)System.out.println("- keluar -");
+            else System.out.println("- Salah -");
+            System.out.println("");
+        }
+    }
+}
+class linked{
+    E31192259_Queue head,tail;
+    public linked(){
+        head=null;
+        tail=null;
+    }
+    public void enque(){
+        E31192259_Queue baru=new E31192259_Queue();
+        baru.input();
+        if(head==null)head=baru;
+        else tail.next=baru;
+        tail=baru;
+    }
+    public void deque(){
+        if(head==null)System.out.println("- Kosong -");
+        else{
+            System.out.println("Keluar Data Dengan Id : "+head.id);
+            head=head.next;
+        }
+    }
+    public void view(){
+        if(head==null)System.out.println("- Kosong -");
+        else{
+            System.out.println("|| Id \t|| Nama \t|| Keperluan \t||");
+            for(E31192259_Queue a=head; a!=null; a=a.next) a.read();
+        }
+    }
+}
